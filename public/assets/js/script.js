bd9881c8d85a9c97b8e6f7b2035acb07d545922c
Simon Hong <simon_hong1@yahoo.com>
2019-02-06 15:11:50 -0700

    updated code

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
-  $(".devour").on("submit", function(event){
-      event.preventDefault()
-      var burger_id=$(this).children(".burger_id").val()
-      $.ajax({
-          method:"PUT",
-          url:"/burgers/" + burger_id
-      }).then(function(data){
-          location.reload()
-      })
-  })
+$(".devour").on("submit", function(event) {
+  event.preventDefault();
+
+  var burger_id = $(this)
+    .children(".burger_id")
+    .val();
+  console.log("this is the id", burger_id);
+  $.ajax("/burgers" + burger_id, {
+    type: "DELETE"
+  }).then(function() {
+      console.log("deleted id" + burger_id);
+    location.reload();
+  });
 });
